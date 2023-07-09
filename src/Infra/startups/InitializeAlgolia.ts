6a3fa0d7424688b44e92e1ac8a191f5bde00b570
ibarretom <igorbm.23@gmail.com>
2023-07-09 16:43:54 -0300

    remoção de console.log

--- src/Infra/startups/InitializeAlgolia.ts
+++ src/Infra/startups/InitializeAlgolia.ts
@@ -4,11 +4,9 @@ import 'dotenv/config'
 export async function InitializeAlgolia() {
   const index = AlgoliaClient.initIndex(process.env.INDEX_NAME as string)
 
-  const response = await index.setSettings({
+  await index.setSettings({
     searchableAttributes: ['titulo_post', 'titulo_do_texto', 'content'],
     highlightPreTag: '**',
     highlightPostTag: '**',
   })
-
-  console.log(response)
 }
